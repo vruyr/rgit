f638c3731e89af4b7d5ad72bab6ecc4a2a711131
Vruyr Gyolchanyan <vruyr@vruyr.com>
2017-12-17 11:04:05 -0500

    Converted settings to use pathlib.

--- repos/settings.py
+++ repos/settings.py
@@ -1,23 +1,20 @@
-import os, json
+import os, json, pathlib
 from . import constants
 
 
 # TODO Respect the options.
-settings_file_path = os.path.join(
-	os.path.expanduser("~"),
-	"." + constants.SELF_NAME + ".json"
-)
+settings_file_path = pathlib.Path.home() / ("." + constants.SELF_NAME + ".json")
 
 
 async def load():
-	if not os.path.exists(settings_file_path):
+	if not settings_file_path.exists():
 		return None
-	with open(settings_file_path, "r") as fo:
+	with settings_file_path.open("r") as fo:
 		config = json.load(fo)
-	config["path"] = settings_file_path
+	config["path"] = os.fspath(settings_file_path)
 	return config
 
 
 async def save(opts, config):
-	with open(settings_file_path, "w") as fo:
+	with settings_file_path.open("w") as fo:
 		json.dump(config, fo, indent="\t")
